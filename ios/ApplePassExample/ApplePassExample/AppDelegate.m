52ffc66a7e9212f0369c9f95ade903d8f89dcff8
Justin Imhoff <justin@justinimhoff.com>
2016-10-13 16:15:57 -0600

    Initial Commit
    Initial Commit

---
+++ ios/ApplePassExample/ApplePassExample/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  ApplePassExample
+//
+//  Created by justin imhoff on 10/13/16.
+//  Copyright © 2016 Relevant Solutions. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
---
+++ ios/ApplePassExample/ApplePassExample/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  ApplePassExample
+//
+//  Created by justin imhoff on 10/13/16.
+//  Copyright © 2016 Relevant Solutions. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+#import <PassKit/PassKit.h>
+
+@interface ViewController ()<WKNavigationDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    WKWebViewConfiguration *theConfiguration = [[WKWebViewConfiguration alloc] init];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:theConfiguration];
+    webView.navigationDelegate = self;
+    [self.view addSubview:webView];
+    
+    //Make sure to provide your API key and Latitude / Longitude if possible or remove paramaters
+    NSMutableURLRequest *relevantSolutionsCoupon = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://web.groupinterest.com/define?&apiKey=&latitude=0&longitude=-0"]];
+    //Request load
+    [webView loadRequest:relevantSolutionsCoupon];
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    if ([navigationResponse.response.MIMEType isEqualToString:@"application/vnd.apple.pkpass"]) {
+        NSURLSessionDataTask *downloadTask = [[NSURLSession sharedSession] dataTaskWithURL:navigationResponse.response.URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            PKPass *pass = [[PKPass alloc] initWithData:data error:&error];
+            if (error) {
+                NSLog(@"Error: %@", error);
+            } else {
+                PKAddPassesViewController *apvc = [[PKAddPassesViewController alloc] initWithPass:pass];
+                [self presentViewController:apvc animated:YES completion:nil];
+            }
+        }];
+        [downloadTask resume];
+        //Stop the loading because it will be a background task
+        decisionHandler(WKNavigationResponsePolicyCancel);
+    }else{
+        //Continue with load
+        decisionHandler(WKNavigationResponsePolicyAllow);
+    }
+}
+
+
+
+@end
